bf9287ab1b62938e7ab6d5bb6776a4063a5e47e3
KT <tran.nk7@gmail.com>
2021-03-01 09:53:02 -0800

    2021.03.01 add comments in UI

--- shinyApp/ui.R
+++ shinyApp/ui.R
@@ -4,6 +4,8 @@
 ui<- shinyUI(fluidPage(
   #theme = shinytheme('slate'),
   titlePanel("Exploring a Subset of MIMIC-III Data"),
+  
+  
   sidebarLayout(
     sidebarPanel(
       # fileInput('file1', 'Choose CSV File',
@@ -18,24 +20,40 @@ ui<- shinyUI(fluidPage(
       h5(paste0('1. Summarizes and plots the distribution of the chosen variable, compatible with both categorical or continuous data')),
       h5(paste0('2. Fits a logitical regression model and plots the odd ratio(s) to quantify the association of the chosen variable with 30-day mortality'))
     ),
+    
+    
     mainPanel(
       #DT::dataTableOutput('contents'),
       
+      #####################################
+      # PRINT BUGTEST OUTPUT
+      #####################################
+      #verbatimTextOutput('logicTest'),
       
+      #####################################
+      # PRINT ERROR MESSAGE
+      #####################################
       conditionalPanel(condition = "output.warnstat == 'Error'",
                        verbatimTextOutput("warnmsg")
                        ),
       
+      #####################################
+      # SUMMARY + HISTOGRAM
+      #####################################
       h4(paste0('1. Distribution of the Chosen Variable')),
+      
       verbatimTextOutput('summary'),
       
       plotOutput('plot'),
       
+      #####################################
+      # LR FIT
+      #####################################
+      
       h4(paste0('2. Univariate Logistic Regression Fit')),
       
-      #verbatimTextOutput('logicTest'),
-      
       verbatimTextOutput('LRfit'),
+      
       plotOutput('LRplot'),
     )
   )
